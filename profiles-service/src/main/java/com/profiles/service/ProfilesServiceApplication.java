285662b7ba6a689f960cbfc8c02ed63a88a2fd36
Mauricio Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2025-06-12 00:07:24 -0500

    feat: implement user profile management Microservice

---
+++ profiles-service/src/main/java/com/profiles/service/ProfilesServiceApplication.java
@@ -0,0 +1,15 @@
+package com.profiles.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ProfilesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesServiceApplication.class, args);
+	}
+
+}
